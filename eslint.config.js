08fb0798fafa38961edaad6d098044a8aceb8395
woojinnnnnnn <ujins8201@gmail.com>
2026-07-02 12:02:47 +0900

    Add TypeScript ESLint support and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default [
   { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [js.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
